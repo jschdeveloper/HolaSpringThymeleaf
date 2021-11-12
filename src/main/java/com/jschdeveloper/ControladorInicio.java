4833cd3ccfe573cefcc95e50c0e27f3e2d91e091
Jesus Sánchez <jesus.sanchez@in-storemedia.com>
2021-11-12 00:30:03 -0600

    se leen archivos desde el properties

--- src/main/java/com/jschdeveloper/ControladorInicio.java
+++ src/main/java/com/jschdeveloper/ControladorInicio.java
@@ -1,6 +1,8 @@
 package com.jschdeveloper;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -8,10 +10,18 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @Slf4j
 public class ControladorInicio {
+	
+	@Value(value = "${index.saludos}")
+	private String saludo;
 
 	@GetMapping("/")
-	public String inicio() {
+	public String inicio(Model model) {
 		log.info("Ejecutando controlador Spring MVC");
+		String mensaje = "Mensaje con thymeleaf:";
+		model.addAttribute("mensaje", mensaje);
+		model.addAttribute("saludo", saludo);
+
+
 		return "index";
 	}
 
