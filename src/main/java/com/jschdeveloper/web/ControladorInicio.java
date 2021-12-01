d754300ff7cc44f78716f16fd089cf71c6d7faf4
Jesus Sánchez <jesus.sanchez@in-storemedia.com>
2021-12-01 17:37:09 -0600

    feat: add delete and edit actions

--- src/main/java/com/jschdeveloper/web/ControladorInicio.java
+++ src/main/java/com/jschdeveloper/web/ControladorInicio.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.jschdeveloper.domain.Persona;
 import com.jschdeveloper.service.PersonaService;
@@ -29,6 +30,35 @@ public class ControladorInicio {
 		model.addAttribute("personas", pesonas);
 
 		return "index";
+	}
+
+	@GetMapping("/agregar")
+	public String agregar(Persona persona) {
+		return "modificar";
+	}
+
+	@PostMapping("/guardar")
+	public String guardar(Persona persona) {
+		personaService.guardar(persona);
+
+		return "redirect:/";
+
+	}
+
+	@GetMapping("/editar/{idPersona}")
+	public String editar(Persona persona, Model model) {
+		persona = personaService.encontrarPersona(persona);
+
+		model.addAttribute("persona", persona);
+
+		return "modificar";
+	}
+
+	@GetMapping("eliminar")
+	public String eliminar(Persona persona, Model model) {
+		personaService.eliminar(persona);
+
+		return "redirect:/";
 	}
 
 	@GetMapping("/health")
