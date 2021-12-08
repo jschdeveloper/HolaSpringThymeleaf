1e9289db065c40c34e78a7b086f70e1c89efb2f6
Jesus Sánchez <jesus.sanchez@in-storemedia.com>
2021-12-08 09:54:59 -0600

    feat: add user in memory and logout

---
+++ src/main/java/com/jschdeveloper/web/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.jschdeveloper.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()//
+				.withUser("admin")//
+				// no encripta el password
+				.password("{noop}123")//
+				.roles("ADMIN", "USER")//
+				.and()//
+				.withUser("user")//
+				.password("{noop}123")//
+				.roles("USER");
+
+	}
+
+}
